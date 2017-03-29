07b566281b35a179b7c3a5053885f9ec6ae77b1f
dzyomaArtem <dz.art.23@gmail.com>
2017-03-29 15:45:39 +0300

    Add Task 5.4

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,8 @@ class User < ActiveRecord::Base
 
   validates :email, :username, uniqueness: true, presence: true
   validates :password, length: { minimum: 8 }, presence: true
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
 end
